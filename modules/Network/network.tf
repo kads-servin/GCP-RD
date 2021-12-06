6966c24abb9ad16fe3a7051c5f32b663a367a73e
karen-servin <kads.servin@gmail.com>
2021-12-06 15:03:25 -0600

    network script

--- modules/Network/network.tf
+++ modules/Network/network.tf
@@ -1,4 +1,9 @@
 resource "google_compute_network" "new-network"{
     name = "new-network"
-auto_creat_subnetworks = false
+    auto_create_subnetworks = false
+    
+}
+
+output "custom"{
+    value = google_compute_network.new-network.id
 }
