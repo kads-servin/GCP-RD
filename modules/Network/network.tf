84099c1e0c1ff20771e8c225b84b0776d19d7c08
karen-servin <kads.servin@gmail.com>
2021-12-06 15:26:06 -0600

    agregue subnet

--- modules/Network/network.tf
+++ modules/Network/network.tf
@@ -5,6 +5,13 @@ resource "google_compute_network" "new-network"{
     
 }
 
+resource "google_compute_subnetwork" "sub-of-newnetwork"{
+    name = "sub-of-newnetwork"
+    network = google_compute_network.new-network.id
+    ip_cidr_range = "10.1.0.0/21"
+    region = "us-central-1"
+}
+
 output "custom"{
     value = google_compute_network.new-network.id
 }
