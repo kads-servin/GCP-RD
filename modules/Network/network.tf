0906f997505cf1f6eae31aa2fa7191551202629f
karen-servin <kads.servin@gmail.com>
2021-12-06 17:19:52 -0600

    add varibale tf network

--- modules/Network/network.tf
+++ modules/Network/network.tf
@@ -1,17 +1,25 @@
 resource "google_compute_network" "new-network"{
-    project = "rd-gcp-kads"
-    name = "new-network"
+    project = var.project_id
+    name = var.vpc_name
     auto_create_subnetworks = false
     
 }
 
 resource "google_compute_subnetwork" "sub-of-newnetwork"{
-    name = "sub-of-newnetwork"
+    name = var.subnet_name
     network = google_compute_network.new-network.id
-    ip_cidr_range = "10.1.0.0/21"
-    region = "us-central1"
+    ip_cidr_range = var.ip_cidr_range
+    region = var.region
 }
 
 output "custom"{
     value = google_compute_network.new-network.id
+}
+
+output "VPC"{
+    value = google_compute_network.new-network.name
+}
+
+output "Subnet"{
+    value = google_compute_subnetwork.sub-of-newnetwork.name
 }
---
+++ modules/Network/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+    default = "rd-gcp-kads"
+}
+
+variable "vpc_name" {
+    default = "new-network"
+}
+
+variable "subnet_name" {
+    default = "new-network"
+}
+
+variable "ip_cidr_range" {
+    default = "10.1.0.0/21"
+}
+
+variable "region" {
+    default = "us-central1"
+}
