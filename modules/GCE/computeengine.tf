c7c327960b44a4f48a3de8cb6da0adad3e929b68
karen-servin <kads.servin@gmail.com>
2021-12-06 16:49:10 -0600

    cambio computeengine tf  cowsay

--- modules/GCE/computeengine.tf
+++ modules/GCE/computeengine.tf
@@ -3,6 +3,7 @@ resource "google_compute_instance" "terraform" {
   name         = "rd-create-instance-kads"
   machine_type = "e2-standard-2"
   zone         = "us-central1-a"
+  metadata_startup_script = "startup.sh"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
