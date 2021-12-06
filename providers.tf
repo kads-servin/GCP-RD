c00894addca260446b32a814843192739730348f
karen-servin <kads.servin@gmail.com>
2021-12-06 13:41:49 -0600

    Primer commit

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.2.1"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "rd-gcp-kads"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "keys.json"
+}
