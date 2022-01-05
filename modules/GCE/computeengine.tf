c8d2260af7ba8efbe130ad2177d5f63662299cdc
karen-servin <kads.servin@gmail.com>
2022-01-05 11:28:29 -0600

    adding the service account

--- modules/GCE/computeengine.tf
+++ modules/GCE/computeengine.tf
@@ -1,10 +1,16 @@
+resource "google_service_account" "service_account" {
+  account_id   = var.service_account
+  display_name = var.service_account
+  project      = var.project_id
+}
+
 resource "google_compute_instance" "terraform" {
   project      = var.project_id
   name         = lower(var.name)
   machine_type = var.machine_type
   zone         = var.zone
   metadata = {
-  startup-script = file("${path.module}/startup.sh")
+    startup-script = file("${path.module}/startup.sh")
   }
   tags = ["demo-vm-instance"]
   boot_disk {
@@ -13,11 +19,16 @@ resource "google_compute_instance" "terraform" {
     }
   }
   network_interface {
-    network = var.vpc_name
+    network    = var.vpc_name
     subnetwork = var.subnet_name
 
     access_config {
     }
 
+  }
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.service_account.email
+    scopes = ["cloud-platform"]
   }
 }
