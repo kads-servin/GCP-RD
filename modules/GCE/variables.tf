68cc867f73ec97860c74ac3b867f924961a8e4c2
karen-servin <kads.servin@gmail.com>
2022-01-05 12:38:54 -0600

    adding the service account to variables

--- modules/GCE/variables.tf
+++ modules/GCE/variables.tf
@@ -1,33 +1,38 @@
 variable "project_id" {
-  default     = "rd-gcp-kads"
-  description = "Project Id in GCP"
+    default = "rd-gcp-kads"
+    description = "Project Id in GCP"
 }
 
 variable "name" {
-  default     = "rd-create-instance-kads"
-  description = "Name of the VM"
+    default = "rd-create-instance-kads"
+    description = "Name of the VM"
 }
 
 variable "machine_type" {
-  default     = "e2-standard-2"
-  description = "Type of machine that we are going to create"
+    default = "e2-standard-2"
+    description = "Type of machine that we are going to create"
 }
 
 variable "zone" {
-  default     = "us-central1-a"
-  description = "Type of zone that we are going to define"
+    default = "us-central1-a"
+    description = "Type of zone that we are going to define"
 }
 
 variable "image" {
-  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
-  description = "Ubuntu version that we are going to create"
+    default = "ubuntu-os-cloud/ubuntu-2004-lts"
+    description = "Ubuntu version that we are going to create"
 }
 variable "vpc_name" {
-  default     = "new-network"
-  description = "Name of the network"
+    default = "new-network"
+    description = "Name of the network"
 }
 
 variable "subnet_name" {
-  default     = "Name of the network"
+  default = "Name of the network"
   description = "Name of the network"
+}
+
+variable "service_account" {
+  default = "service_account"
+  description = "Name of the service account"
 }
