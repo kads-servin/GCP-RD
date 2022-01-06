218c35b39823c1caa61cb6bee7706e8eda51a7b0
karen-servin <kads.servin@gmail.com>
2022-01-06 12:05:57 -0600

    changing startup.sh

--- modules/GCE/startup.sh
+++ modules/GCE/startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-sudo apt update
-sudo apt -y install cowsay
-sudo /usr/games/cowsay "Look My Cowsay" | sudo tee /tmp/message.txt
+sudo apt update 
+
+sudo touch /tmp/pubsub.sh
+
+sudo cat << 'EOF' > /tmp/pubsub.sh
+#!/bin/bash
+sudo gcloud pubsub subscriptions pull suscription-demo --auto-ack --format='json' | tee -a /tmp/message-DEMO-KADS-$(date +'%d-%m-%Y-%H:%M').json
+sudo gsutil cp /tmp/message-DEMO-KADS-*.json gs://kads_demo_bucket-1/PubSubMessages
+rm -r message*
+EOF
+
+sudo chmod +x /tmp/pubsub.sh
+sudo echo "*/6 * * * * /tmp/pubsub.sh" | /usr/bin/crontab
